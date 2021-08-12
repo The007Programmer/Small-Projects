76198d4067ae53fa3fce4f58cfffd8a05312074d
MilkshakeTheCoder <aahils2019@gmail.com>
2021-08-11 17:00:31 -0700

    final presidential gen

--- Presidential-Generator/presidentialgenerator.py
+++ Presidential-Generator/presidentialgenerator.py
@@ -1,11 +1,15 @@
 from WhiteHouse import p, vp
 import random
+from utils import jsonloader
 
 while True:
     prezlist=["Gamefan586", "Epic Gamer"]
     randomprez=input("Keyword to start > ")
-    if randomprez=="投票4プレズを開始":
+    secret_file = jsonloader.read_json("prezsecrets")
+    keyword=secret_file["PrezGenTOKEN"]
+    if randomprez==keyword:
         print("Starting Generator...")
         print(f"The President is {random.choice(prezlist)}")
+        break
     else:
         print(f"{randomprez} is the incorrect Starting Key...")
